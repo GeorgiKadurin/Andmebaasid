7129ca424f74e6a4979271737a85f9a146e27d9f
Opilane TTHK <georgi.kkadurin@gmail.com>
2024-04-08 14:53:21 +0300

    Insert into on valmis

--- tabeliloomine.sql
+++ tabeliloomine.sql
@@ -25,3 +25,35 @@ VALUES(
 
 SELECT * FROM opilane;
 
+-----------------------------------------------------------------------------------------------------
+
+CREATE TABLE Language
+(
+ID int NOT NULL PRIMARY KEY,
+Code char(3) NOT NULL,
+Language varchar(50) NOT NULL,
+IsOfficial bit,
+Percentage decimal(5,2)
+); 
+
+--tabeli andmete lisamine 
+SELECT * FROM Language;
+INSERT INTO Language (ID, Code, Language, IsOfficial, Percentage)
+VALUES (100, 'EST', 'Eesti', 1, 80.5)
+
+--Tabeli struktuuri muutmine
+ALTER TABLE Language ADD Capital varchar(20);
+
+--Veergu kustutamine
+ALTER TABLE Language DROP COLUMN Capital;
+
+INSERT INTO Language (ID, Code, Language, IsOfficial, Percentage, Capital )
+VALUES (101, 'FIM', 'Soome', 1, 96.5, 'Helsinki');
+
+--uuendamine
+UPDATE Language SET Capital='Tallinn'
+WHERE ID=100
+
+
+--TABLE kustutamine
+DROP TABLE Language;
