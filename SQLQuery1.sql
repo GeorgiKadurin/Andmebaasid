9c85f8393674e265bb0964f402f5b796e6eaedc2
GeorgiKadurin <georgi.kkadurin@gmail.com>
2024-05-08 13:00:17 +0300

    db loomine

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -194,7 +194,7 @@ Salary nvarchar(50),
 DepartmentId int
 )
 
---?
+--Täidab tabeli Department ja Employees
 insert into Department (Id, DepartmentName, Location, DepartmentHead)
 values (1, 'IT', 'London', 'Rick')
 insert into Department (Id, DepartmentName, Location, DepartmentHead)
